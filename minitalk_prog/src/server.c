4b5789d365c3582454412955dcc4ea8050b7ead4
Mounas Ali <mounali@c2r8p5.42perpignan.fr>
2024-01-07 17:31:18 +0100

    ok

---
+++ minitalk_prog/src/server.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mounali <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 13:50:01 by mounali           #+#    #+#             */
+/*   Updated: 2023/03/30 11:05:19 by mounali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../includes/minitalk.h"
+
+void	ft_btoa(int sig)
+{
+	static int	bit;
+	static int	i;
+
+	if (sig == SIGUSR1)
+		i |= (0x01 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		ft_printf("%c", i);
+		bit = 0;
+		i = 0;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	(void)argv;
+	if (argc != 1)
+	{
+		ft_printf("Error\n");
+		return (1);
+	}
+	pid = getpid();
+	ft_printf("%d\n", pid);
+	while (argc == 1)
+	{
+		signal(SIGUSR1, ft_btoa);
+		signal(SIGUSR2, ft_btoa);
+	}
+	while (1)
+		pause();
+	return (0);
+}
